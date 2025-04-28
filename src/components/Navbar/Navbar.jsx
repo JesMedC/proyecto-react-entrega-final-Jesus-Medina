b8de87f4b043c37f99f30e1f017b865ed057e235
Jesus Medina <155578543+JesMedC@users.noreply.github.com>
2025-04-28 03:36:19 -0400

    Agregando CartWidget

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../CartContext/CartContext'; // Importamos el contexto
-import CartWidget from '../CartWidget/CartWidget';
 import './Navbar.css';
 
 function Navbar() {
